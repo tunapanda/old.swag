a48d403e49104208231440f6497c29f9a8731ab2
Connor James Leech <connorleech@gmail.com>
2014-08-04 13:40:41 -0700

    init angular-fullstack

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('swagApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
